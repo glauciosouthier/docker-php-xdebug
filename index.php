a0978833969c3962772b0c189187ad4880efc5ad
glaucio.southier <glaucio.southier@cjf.jus.br>
2020-03-17 23:32:53 -0300

    conf

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php header('Content-Type: text/html; charset=iso-8859-1'); ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+ <body>
+ <p>Olá Mundo</p>
+ <?php 
+ 
+ echo "<p>Olá Mundo</p>";
+ 
+ ?>
+ </body>
+</html>
